24b61727708b344780d54e72e2a0da6a166cdbc1
ffetterr <92558860+ffetterr@users.noreply.github.com>
2022-03-04 00:06:34 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,164 @@
+import pygame
+import random
+ 
+# инициализировать pygame
+pygame.init()
+ 
+# создать отображение & запустить обновление
+width = 640
+height = 480
+display = pygame.display.set_mode((width, height))
+ 
+pygame.display.update()
+pygame.display.set_caption("Игра змейка by Shatunov and Sviridov")
+ 
+# определение цветов
+colors = {
+    "snake_head": (60, 121, 250),
+    "snake_tail": (70, 190, 219),
+    "apple": (242, 109, 116)
+}
+ 
+# положение змеи стрелочками
+snake_pos = {
+    "x": width/2-10,
+    "y": height/2-10,
+    "x_change": 0,
+    "y_change": 0
+}
+ 
+# размер змеи
+snake_size = (10, 10)
+ 
+# текущая скорость движения змеи
+snake_speed = 10
+ 
+# Змеиные хвосты
+snake_tails = []
+ 
+snake_pos["x_change"] = -snake_speed
+for i in range(75):
+    snake_tails.append([snake_pos["x"] + 10*i, snake_pos["y"]])
+ 
+# еда
+food_pos = {
+    "x": round(random.randrange(0, width - snake_size[0]) / 10) * 10,
+    "y": round(random.randrange(0, height - snake_size[1]) / 10) * 10,
+}
+ 
+food_size = (10, 10)
+food_eaten = 0
+ 
+# начальный цикл
+game_end = False
+clock = pygame.time.Clock()
+ 
+while not game_end:
+    # игровой цикл
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_end = True
+ 
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT and snake_pos["x_change"] == 0:
+                # движение влево
+                snake_pos["x_change"] = -snake_speed
+                snake_pos["y_change"] = 0
+ 
+            elif event.key == pygame.K_RIGHT and snake_pos["x_change"] == 0:
+                # движение вправо
+                snake_pos["x_change"] = snake_speed
+                snake_pos["y_change"] = 0
+ 
+            elif event.key == pygame.K_UP and snake_pos["y_change"] == 0:
+                # движение вверх
+                snake_pos["x_change"] = 0
+                snake_pos["y_change"] = -snake_speed
+ 
+            elif event.key == pygame.K_DOWN and snake_pos["y_change"] == 0:
+                # движение вниз
+                snake_pos["x_change"] = 0
+                snake_pos["y_change"] = snake_speed
+    # очиститка экран
+    display.fill((0,0,0))
+ 
+ 
+    # движение змеиных хвостов
+    ltx = snake_pos["x"]
+    lty = snake_pos["y"]
+ 
+    for i,v in enumerate(snake_tails):
+        _ltx = snake_tails[i][0]
+        _lty = snake_tails[i][1]
+ 
+        snake_tails[i][0] = ltx
+        snake_tails[i][1] = lty
+ 
+        ltx = _ltx
+        lty = _lty
+ 
+    # визуализация хвотов змейки
+    for t in snake_tails:
+        pygame.draw.rect(display, colors["snake_tail"], [
+            t[0],
+            t[1],
+            snake_size[0],
+            snake_size[1]])
+ 
+    # визуализация головы змеи
+    snake_pos["x"] += snake_pos["x_change"]
+    snake_pos["y"] += snake_pos["y_change"]
+ 
+    # телепорт змеи, если та вышла за пределы
+    if(snake_pos["x"] < -snake_size[0]):
+        snake_pos["x"] = width
+ 
+    elif(snake_pos["x"] > width):
+        snake_pos["x"] = 0
+ 
+    elif(snake_pos["y"] < -snake_size[1]):
+        snake_pos["y"] = height
+ 
+    elif(snake_pos["y"] > height):
+        snake_pos["y"] = 0
+ 
+    pygame.draw.rect(display, colors["snake_head"], [
+        snake_pos["x"],
+        snake_pos["y"],
+        snake_size[0],
+        snake_size[1]])
+ 
+    # визуализация яблока
+    pygame.draw.rect(display, colors["apple"], [
+        food_pos["x"],
+        food_pos["y"],
+        food_size[0],
+        food_size[1]])
+ 
+    # столкнулась с яблоком = скушала
+    if(snake_pos["x"] == food_pos["x"]
+        and snake_pos["y"] == food_pos["y"]):
+        food_eaten += 1
+        snake_tails.append([food_pos["x"], food_pos["y"]])
+ 
+        food_pos = {
+            "x": round(random.randrange(0, width - snake_size[0]) / 10) * 10,
+            "y": round(random.randrange(0, height - snake_size[1]) / 10) * 10,
+        }
+ 
+    # столкнулась с хвостом = уменьшение змеи
+    for i,v in enumerate(snake_tails):
+        if(snake_pos["x"]+snake_pos["x_change"] == snake_tails[i][0]
+            and snake_pos["y"]+snake_pos["y_change"] == snake_tails[i][1]):
+            snake_tails = snake_tails[:i]
+            break
+ 
+    pygame.display.update()
+    
+    # количество кадров в секунду
+    clock.tick(30)
+    
+ 
+# выход
+pygame.quit()
+quit()
